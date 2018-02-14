f70d016bfc3e5cf248be43a3d8edd321c105e53e
michael.englert <michi.eng@gmail.com>
2018-02-14 20:50:02 +0100

    env variable fix

--- appd_env.sh
+++ appd_env.sh
@@ -1,11 +1,11 @@
-APPDYNAMICS_CONTROLLER_HOST_NAME=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_CONTROLLER_HOST_NAME)
-APPDYNAMICS_CONTROLLER_PORT=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_CONTROLLER_PORT)
-APPDYNAMICS_CONTROLLER_SSL_ENABLED=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_CONTROLLER_SSL_ENABLED)
-APPDYNAMICS_AGENT_ACCOUNT_NAME=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_AGENT_ACCOUNT_NAME)
-APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY)
-APPDYNAMICS_AGENT_APPLICATION_NAME=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_AGENT_APPLICATION_NAME)
-APPDYNAMICS_AGENT_TIER_NAME=$(/opt/elasticbeanstalk/bin/get-config container -k environment_name)
-APPDYNAMICS_SIM_ENABLED=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_SIM_ENABLED)
+export APPDYNAMICS_CONTROLLER_HOST_NAME=$(sudo /opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_CONTROLLER_HOST_NAME)
+export APPDYNAMICS_CONTROLLER_PORT=$(sudo /opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_CONTROLLER_PORT)
+export APPDYNAMICS_CONTROLLER_SSL_ENABLED=$(sudo /opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_CONTROLLER_SSL_ENABLED)
+export APPDYNAMICS_AGENT_ACCOUNT_NAME=$(sudo /opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_AGENT_ACCOUNT_NAME)
+export APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY=$(sudo /opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY)
+export APPDYNAMICS_AGENT_APPLICATION_NAME=$(sudo /opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_AGENT_APPLICATION_NAME)
+export APPDYNAMICS_AGENT_TIER_NAME=$(sudo /opt/elasticbeanstalk/bin/get-config container -k environment_name)
+export APPDYNAMICS_SIM_ENABLED=$(sudo /opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_SIM_ENABLED)
 
 JAVA_OPTS=$(/opt/elasticbeanstalk/bin/get-config environment -k JAVA_OPTS)
-JAVA_OPTS="$JAVA_OPTS -Dappdynamics.agent.reuse.nodeName=true -Dappdynamics.agent.reuse.nodeName.prefix=$APPDYNAMICS_AGENT_TIER_NAME"
+export JAVA_OPTS="$JAVA_OPTS -Dappdynamics.agent.reuse.nodeName=true -Dappdynamics.agent.reuse.nodeName.prefix=$APPDYNAMICS_AGENT_TIER_NAME"
