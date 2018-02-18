9cd97fbe865dcc16cb2e8956f737b6098d2e4031
Michi <michi.eng@gmail.com>
2018-02-18 09:59:23 +0000

    java only

--- java/appd_env.sh
+++ java/appd_env.sh
@@ -5,7 +5,6 @@ export APPDYNAMICS_AGENT_ACCOUNT_NAME=$(sudo /opt/elasticbeanstalk/bin/get-confi
 export APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY=$(sudo /opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY)
 export APPDYNAMICS_AGENT_APPLICATION_NAME=$(sudo /opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_AGENT_APPLICATION_NAME)
 export APPDYNAMICS_AGENT_TIER_NAME=$(sudo /opt/elasticbeanstalk/bin/get-config container -k environment_name)
-export APPDYNAMICS_SIM_ENABLED=$(sudo /opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_SIM_ENABLED)
 
 JAVA_OPTS=$(sudo /opt/elasticbeanstalk/bin/get-config environment -k JAVA_OPTS)
 export JAVA_OPTS="$JAVA_OPTS -Dappdynamics.agent.reuse.nodeName=true -Dappdynamics.agent.reuse.nodeName.prefix=$APPDYNAMICS_AGENT_TIER_NAME"
