ecf622c84ead37827a58058729c23bd02954cb98
Michi <michi.eng@gmail.com>
2018-02-25 21:13:27 +0000

    Bug fixes

--- java/appd_bashrc.sh
+++ java/appd_bashrc.sh
@@ -1,16 +1,16 @@
 #TODO
 #Clean /etc/bash.bashrc
 
-APPDYNAMICS_CONTROLLER_HOST_NAME=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_CONTROLLER_HOST_NAME > /dev/null 2>&1)
-APPDYNAMICS_CONTROLLER_PORT=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_CONTROLLER_PORT > /dev/null 2>&1)
-APPDYNAMICS_CONTROLLER_SSL_ENABLED=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_CONTROLLER_SSL_ENABLED > /dev/null 2>&1)
-APPDYNAMICS_AGENT_ACCOUNT_NAME=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_AGENT_ACCOUNT_NAME > /dev/null 2>&1)
-APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY > /dev/null 2>&1)
-APPDYNAMICS_AGENT_APPLICATION_NAME=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_AGENT_APPLICATION_NAME > /dev/null 2>&1)
-APPDYNAMICS_AGENT_TIER_NAME=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_AGENT_TIER_NAME > /dev/null 2>&1)
-JAVA_OPTS=$(/opt/elasticbeanstalk/bin/get-config environment -k JAVA_OPTS > /dev/null 2>&1)
+APPDYNAMICS_CONTROLLER_HOST_NAME=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_CONTROLLER_HOST_NAME 2>&1)
+APPDYNAMICS_CONTROLLER_PORT=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_CONTROLLER_PORT 2>&1)
+APPDYNAMICS_CONTROLLER_SSL_ENABLED=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_CONTROLLER_SSL_ENABLED 2>&1)
+APPDYNAMICS_AGENT_ACCOUNT_NAME=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_AGENT_ACCOUNT_NAME <2>&1)
+APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY 2>&1)
+APPDYNAMICS_AGENT_APPLICATION_NAME=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_AGENT_APPLICATION_NAME 2>&1)
+APPDYNAMICS_AGENT_TIER_NAME=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_AGENT_TIER_NAME 2>&1)
+JAVA_OPTS=$(/opt/elasticbeanstalk/bin/get-config environment -k JAVA_OPTS 2>&1)
 
-if [ -z APPDYNAMICS_CONTROLLER_HOST_NAME ]
+if [[ -v APPDYNAMICS_CONTROLLER_HOST_NAME ]]
 then
     echo "APPDYNAMICS_CONTROLLER_HOST_NAME=$APPDYNAMICS_CONTROLLER_HOST_NAME" >> /etc/bash.bashrc
 else
@@ -18,7 +18,7 @@ else
     exit 1
 fi
 
-if [ -z APPDYNAMICS_CONTROLLER_PORT ]
+if [[ -v APPDYNAMICS_CONTROLLER_PORT ]]
 then
     echo "APPDYNAMICS_CONTROLLER_PORT=$APPDYNAMICS_CONTROLLER_PORT" >> /etc/bash.bashrc
 else
@@ -26,7 +26,7 @@ else
     exit 1
 fi
 
-if [ -z APPDYNAMICS_CONTROLLER_SSL_ENABLED ]
+if [[ -v APPDYNAMICS_CONTROLLER_SSL_ENABLED ]]
 then
     echo "APPDYNAMICS_CONTROLLER_SSL_ENABLED=$APPDYNAMICS_CONTROLLER_SSL_ENABLED" >> /etc/bash.bashrc
 else
@@ -34,7 +34,7 @@ else
     echo "APPDYNAMICS_CONTROLLER_SSL_ENABLED=false" >> /etc/bash.bashrc
 fi
 
-if [ -z APPDYNAMICS_AGENT_ACCOUNT_NAME ]
+if [[ -v APPDYNAMICS_AGENT_ACCOUNT_NAME ]]
 then
     echo "APPDYNAMICS_AGENT_ACCOUNT_NAME=$APPDYNAMICS_AGENT_ACCOUNT_NAME" >> /etc/bash.bashrc
 else
@@ -42,7 +42,7 @@ else
     echo "APPDYNAMICS_AGENT_ACCOUNT_NAME=customer1" >> /etc/bash.bashrc
 fi
 
-if [ -z APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY ]
+if [[ -v APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY ]]
 then
     echo "APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY=$APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY" >> /etc/bash.bashrc
 else
@@ -50,7 +50,7 @@ else
     exit 1
 fi
 
-if [ -z APPDYNAMICS_AGENT_APPLICATION_NAME ]
+if [[ -v APPDYNAMICS_AGENT_APPLICATION_NAME ]]
 then
     echo "APPDYNAMICS_AGENT_APPLICATION_NAME=$APPDYNAMICS_AGENT_APPLICATION_NAME" >> /etc/bash.bashrc
 else
@@ -58,7 +58,7 @@ else
     exit 1
 fi
 
-if [ -z APPDYNAMICS_AGENT_TIER_NAME ]
+if [[ -v APPDYNAMICS_AGENT_TIER_NAME ]]
 then
     echo "APPDYNAMICS_AGENT_TIER_NAME=$APPDYNAMICS_AGENT_TIER_NAME" >> /etc/bash.bashrc
 else
@@ -66,7 +66,7 @@ else
     echo "APPDYNAMICS_AGENT_TIER_NAME=$(/opt/elasticbeanstalk/bin/get-config container -k environment_name > /dev/null 2>&1)" >> /etc/bash.bashrc
 fi
 
-if [ -z JAVA_OPTS ]
+if [[ -v JAVA_OPTS ]]
 then
     echo "JAVA_OPTS=$JAVA_OPTS -Dappdynamics.agent.reuse.nodeName=true -Dappdynamics.agent.reuse.nodeName.prefix=$APPDYNAMICS_AGENT_TIER_NAME" >> /etc/bash.bashrc
 else
--- testing/test.sh
+++ testing/test.sh
@@ -6,7 +6,19 @@ KEY="aws-eb-yoga"
 REGION="us-west-2"
 INSTANCE="t2.micro"
 PLATFORM="java"
+BASE_DIR="/home/michi/workspace"
 
+cd $BASE_DIR
+rm -rf spring-music
+git clone https://github.com/cloudfoundry-samples/spring-music
+cd spring-music
 eb init $APP_NAME -p $PLATFORM -r $REGION -k $KEY
+echo "build: gradle clean assemble" >> Buildfile
+echo 'web: java $JAVA_OPTS -Dserver.port=8080 -Dspring.profiles.active=in-memory -jar build/libs/spring-music.jar' >> Procfile
+mkdir .ebextensions
+cp -r /home/michi/Documents/business/appd/dev/aws/aws-elasticbeanstalk/java/.ebextensions/nginx ./.ebextensions
+cp /home/michi/Documents/business/appd/dev/aws/aws-elasticbeanstalk/java/.ebextensions/appd_bashrc.config ./.ebextensions
+git add ./
+git commit -m "eb"
 eb create $ENV_NAME -i $INSTANCE
 eb terminate $ENV_NAME --force
