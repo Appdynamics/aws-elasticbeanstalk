d1e26a8973488a2bee5cd738010a78f65792a275
Michi <michi.eng@gmail.com>
2018-02-27 10:16:43 +0000

    Bug fixes

--- java/appd_env.sh
+++ java/appd_env.sh
@@ -8,7 +8,6 @@ APPDYNAMICS_AGENT_ACCOUNT_NAME=$(/opt/elasticbeanstalk/bin/get-config environmen
 APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY 2>&1)
 APPDYNAMICS_AGENT_APPLICATION_NAME=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_AGENT_APPLICATION_NAME 2>&1)
 APPDYNAMICS_AGENT_TIER_NAME=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_AGENT_TIER_NAME 2>&1)
-JAVA_OPTS=$(/opt/elasticbeanstalk/bin/get-config environment -k JAVA_OPTS 2>&1)
 
 if [ -n "${APPDYNAMICS_CONTROLLER_HOST_NAME:+1}" ]
 then
@@ -75,15 +74,6 @@ else
     APPDYNAMICS_AGENT_TIER_NAME=$(/opt/elasticbeanstalk/bin/get-config container -k environment_name 2>&1)
     echo "APPDYNAMICS_AGENT_TIER_NAME=$APPDYNAMICS_AGENT_TIER_NAME" >> /etc/environment
     echo "export APPDYNAMICS_AGENT_TIER_NAME=$APPDYNAMICS_AGENT_TIER_NAME" >> /etc/profile.d/appd_profile.sh
-fi
-
-if [ -n "${JAVA_OPTS:+1}" ]
-then
-    echo "JAVA_OPTS=\"$JAVA_OPTS\"" >> /etc/environment
-    echo "export JAVA_OPTS=\"$JAVA_OPTS\"" >> /etc/profile.d/appd_profile.sh
-else
-    echo "JAVA_OPTS not set. Exiting."
-    exit 1
 fi
 
 exit 0
