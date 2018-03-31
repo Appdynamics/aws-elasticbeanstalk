0d4cb8d98350cef89e7d1db57a3f8892d5d0d15b
Michi <michi.eng@gmail.com>
2018-03-31 09:13:50 +0000

    Added comments to the main script. Added Debug Flag.

--- java/appd.sh
+++ java/appd.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Default Options will be gathered from Elastic Beanstalk
 APPDYNAMICS_CONTROLLER_HOST_NAME=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_CONTROLLER_HOST_NAME 2>&1)
 APPDYNAMICS_CONTROLLER_PORT=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_CONTROLLER_PORT 2>&1)
 APPDYNAMICS_CONTROLLER_SSL_ENABLED=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_CONTROLLER_SSL_ENABLED 2>&1)
@@ -10,10 +11,16 @@ APPDYNAMICS_AGENT_TIER_NAME=$(/opt/elasticbeanstalk/bin/get-config environment -
 APPDYNAMICS_SIM_ENABLED=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_SIM_ENABLED 2>&1)
 APPDYNAMICS_AGENT_GLOBAL_ACCOUNT_NAME=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_AGENT_GLOBAL_ACCOUNT_NAME 2>&1)
 APPDYNAMICS_ANALYTICS_EVENT_ENDPOINT=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_ANALYTICS_EVENT_ENDPOINT 2>&1)
+APPDYNAMICS_DEBUG=$(/opt/elasticbeanstalk/bin/get-config environment -k APPDYNAMICS_DEBUG 2>&1)
+
+# Default Error and Success Messages for the Machine Agent
 MACHINE_ERROR="WARN ExtensionManager - Failed to start extension ServerMonitoring."
 MACHINE_SUCCESS="Started AppDynamics Machine Agent Successfully"
 
+# Deleting any old files
 rm -rf /opt/appdynamics > /dev/null 2>&1
+
+# Check and extraction of the Java Agent
 if [ -f /tmp/appagent.zip ]
 then
     mkdir -p /opt/appdynamics/appagent
@@ -26,6 +33,7 @@ else
     exit 1
 fi
 
+# Environment Variable Checks and persistence
 if [ -n "${APPDYNAMICS_CONTROLLER_HOST_NAME:+1}" ]
 then
     echo "APPDYNAMICS_CONTROLLER_HOST_NAME=$APPDYNAMICS_CONTROLLER_HOST_NAME" >> /etc/environment
@@ -102,6 +110,13 @@ else
     echo "export APPDYNAMICS_AGENT_TIER_NAME=$APPDYNAMICS_AGENT_TIER_NAME" >> /etc/profile.d/appd_profile.sh
 fi
 
+# App Agent Debug Setting
+if [ "$APPDYNAMICS_DEBUG" == "true" ]
+then
+    find /opt/appdynamics/appagent/ -iname log4j.xml -exec sed -i "s@\"info\"@\"debug\"@" {} \;
+fi
+
+# Adding the Java Agent to the Application
 sed -i "s@\(.*java \)\(.*\)@\1-javaagent:/opt/appdynamics/appagent/javaagent.jar -Dappdynamics.agent.reuse.nodeName=true -Dappdynamics.agent.reuse.nodeName.prefix=$APPDYNAMICS_AGENT_TIER_NAME -Dappdynamics.agent.tierName=$APPDYNAMICS_AGENT_TIER_NAME \2@" /var/elasticbeanstalk/staging/supervisor/application.conf
 
 if [ -f /tmp/machineagent.zip ]
@@ -122,6 +137,8 @@ then
         echo "APPDYNAMICS_SIM_ENABLED=false" >> /etc/environment
         echo "export APPDYNAMICS_SIM_ENABLED=false" >> /etc/profile.d/appd_profile.sh
     fi
+
+    # Creation of an intermediate Script that takes care of the Machine Agent Startup
     echo "#!/bin/bash
 
     function checkError {
@@ -154,6 +171,10 @@ then
     exit 0
     " > /opt/appdynamics/appd-machine.sh
 
+    # Making intermediate Script executable
+    chmod 755 /opt/appdynamics/appd-machine.sh
+
+    # If the corresponding Environment Variables are set the Analytics Plugin will be enabled by default
     if [ -n "${APPDYNAMICS_AGENT_GLOBAL_ACCOUNT_NAME:+1}" ] && [ -n "${APPDYNAMICS_ANALYTICS_EVENT_ENDPOINT:+1}" ]
     then
         sed -i "s@false@true@" /opt/appdynamics/machineagent/monitors/analytics-agent/monitor.xml
@@ -166,13 +187,19 @@ then
     else
         echo "AppDynamics Analytics not enabled cause either APPDYNAMICS_AGENT_GLOBAL_ACCOUNT_NAME or APPDYNAMICS_ANALYTICS_EVENT_ENDPOINT is missing."
     fi
-    #sed -i "s@info@debug@" /opt/appdynamics/machineagent/conf/logging/log4j.xml #debug flag
-    chmod 755 /opt/appdynamics/appd-machine.sh
+
+    # Machine Agent Debug Setting
+    if [ "$APPDYNAMICS_DEBUG" == "true" ]
+    then
+        find /opt/appdynamics/machineagent/ -iname log4j.xml -exec sed -i "s@\"info\"@\"debug\"@" {} \;
+    fi
+
     /opt/appdynamics/appd-machine.sh < /dev/null &> /dev/null & disown
 else
     echo "AppDynamics Machine Agent does not exist. Skipping."
 fi
 
+# Making Profile Script executable
 chmod 755 /etc/profile.d/appd_profile.sh
 
 exit 0
